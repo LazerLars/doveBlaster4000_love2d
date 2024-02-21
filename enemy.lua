155efb2c9c1773c55967359b13a3caca08aafa59
Lars Mathiesen <larswmat@gmail.com>
2024-02-21 19:34:06 +0100

    refactored dove move part from update to its own function

--- enemy.lua
+++ enemy.lua
@@ -27,22 +27,14 @@ function enemy.create(x, y)
 end
 
 function enemy.update(dt)
-    --we are looping through it in reverse order, since the remove enemy from list part was causing a issue where the code exploded.
-    -- for index = #enemy.list, 1, -1 do
-    --     local enemyInstance = enemy.list[index]
-    --     local fallSpeed = 50
-    --     local moveSpeed = 100
-    --     -- Update enemy logic
-    --     enemyInstance.x = enemyInstance.x + moveSpeed * dt
-    --     enemyInstance.y = enemyInstance.y + fallSpeed * dt
+    enemy.moveDove(dt)
+end
 
-    --     -- Remove enemy if out of bounds
-    --     if enemyInstance.x < 0 or enemyInstance.x > 128 then
-    --         --table.remove(enemy.list, index)
-    --         enemy.remove(index)
-    --         print('Removing clayDove at index ' .. index)
-    --     end
-    -- end
+function enemy.remove(index)
+    table.remove(enemy.list, index)
+end
+
+function enemy.moveDove(dt)
     for doveIndex, dove in ipairs(enemy.list) do
         local fallSpeed = 50
         local moveSpeed = 100
@@ -59,9 +51,6 @@ function enemy.update(dt)
     end
 end
 
-function enemy.remove(index)
-    table.remove(enemy.list, index)
-end
 
 
 function enemy.draw()
