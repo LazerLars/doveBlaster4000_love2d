ddd393d0e9f135193a0d59b62a6d26276215cac4
Lars Mathiesen <larswmat@gmail.com>
2024-02-21 14:19:09 +0100

    collisons added

--- collision.lua
+++ collision.lua
@@ -12,18 +12,24 @@ function collision.check()
 
 
     for bulletIndex, bullet in ipairs(bullets) do
-        print('bullet ' .. bulletIndex .. ' xy = ' .. bullet.x .. ',' .. bullet.y)
+        --print('bullet ' .. bulletIndex .. ' xy = ' .. bullet.x .. ',' .. bullet.y)
         for doveIndex, dove in ipairs(clayDoves) do
-            print('dove ' .. doveIndex .. ' xy = ' .. dove.x .. ',' .. dove.y)
+            --print('dove ' .. doveIndex .. ' xy = ' .. dove.x .. ',' .. dove.y)
             --correct this so i check for a collision here.. both bullet aand dove have x,y and w and h proptery
              -- Check for collision based on x, y, width, and height
-            if bullet.x < dove.x + dove.w and
-            bullet.x + bullet.w > dove.x and
-            bullet.y < dove.y + dove.h and
-            bullet.y + bullet.h > dove.y then
-                print('collision!!!')
-                print('collision!!!')
+             if bullet.x < dove.x + dove.w and -- Right edge of bullet is to the left of the right edge of dove
+             bullet.x + bullet.w > dove.x and -- Left edge of bullet is to the right of the left edge of dove
+             bullet.y < dove.y + dove.h and -- Bottom edge of bullet is above the top edge of dove
+             bullet.y + bullet.h > dove.y then -- Top edge of bullet is below the bottom edge of dove
+              print('collision!!!')
+              print('collision!!!')
             end
+          
+            -- if math.abs(dove.x - bullet.x) < 0.5 * (dove.w + bullet.w) and 
+            -- math.abs(dove.y - bullet.y) < 0.5 * (dove.h + bullet.h)  then
+            --     print('collision!!!')
+            --     print('collision!!!')
+            -- end
         end
     end
     
