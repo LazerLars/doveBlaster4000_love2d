0bb46bdfc88cd7a250b568966c184630a07bd230
Lars Mathiesen <larswmat@gmail.com>
2024-02-25 21:33:06 +0100

    base for particleSystem

---
+++ UtillityFunctions/simpleParticles.lua
@@ -0,0 +1,15 @@
+--simpleParticles.lua
+
+local simpleParticles = {}
+
+function simpleParticles.load()
+    simpleParticles.list = {}
+end
+
+function simpleParticles.update(dt)
+
+end
+
+function simpleParticles.draw()
+
+end
