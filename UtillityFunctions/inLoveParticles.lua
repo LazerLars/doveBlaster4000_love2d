05504a5df5839f04d375fe68151edaf136ee5afa
Lars Mathiesen <larswmat@gmail.com>
2024-02-26 20:51:28 +0100

    comments added

--- UtillityFunctions/inLoveParticles.lua
+++ UtillityFunctions/inLoveParticles.lua
@@ -4,8 +4,10 @@ local inLoveParticles = {}
 
 function inLoveParticles.load()
     inLoveParticles.list = {} -- list of particles
-    inLoveParticles.createSimple(50, 50)
-    inLoveParticles.createSimple(25, 25)
+    inLoveParticles.createSimple(50, 50, 'right')
+    inLoveParticles.createSimple(50, 50, 'left')
+    inLoveParticles.createSimple(55, 50, 'right')
+    inLoveParticles.createSimple(55, 50, 'left')
 end
 
 function inLoveParticles.update(dt)
@@ -22,31 +24,47 @@ function inLoveParticles.removeParticle(index)
 end
 
 
-function inLoveParticles.createSimple(x, y)
+function inLoveParticles.createSimple(x, y, direction)
     local newParticle = {
         x = x,
         y = y,
-        duration = 0.5
+        duration = 0.5,
+        direction = direction --math.random() < 0.5 and 'left' or 'right'
     }
     table.insert(inLoveParticles.list, newParticle)
 
 end
 
-function inLoveParticles.simpleDropMove(dt, direction)
+function inLoveParticles.simpleDropMove(dt)
     --todo: make particle go a little up, a little left and then drop...
     for particleIndex, particle in ipairs(inLoveParticles.list) do
+        --decrease duration so we racha 0 at some point and can kill it
         particle.duration = particle.duration - dt
-        local direction = math.random(0,1)
+        --if we are above 0.3 secounds left we want the particle to move in a arch form
         if particle.duration >= 0.3 then
-            
-            particle.x = particle.x - math.random(40,100) * dt
+            --move the particle left
+            if particle.direction == 'left' then
+                particle.x = particle.x - math.random(40,300) * dt
+            --else move it right
+            else
+                particle.x = particle.x + math.random(40,200) * dt
+            end
+            --make the particle go upwards
             particle.y = particle.y - math.random(60,120) * dt
+        --if we have less than 0.3 secounds left we want the particle to drop
         else
-            particle.x = particle.x - math.random(12,30) * dt
+            --move the particle left
+            if particle.direction == 'left' then
+                particle.x = particle.x - math.random(12,30) * dt
+            --else move it right
+            else
+                particle.x = particle.x + math.random(12,30) * dt
+            end
+            --make the particle fall (move down)
             particle.y = particle.y + math.random(130,350) * dt
-
         end
 
+        --if the duration is less than 0 we want to remove it
         if particle.duration <= 0 then
             print('die particle')
             inLoveParticles.removeParticle(particleIndex)
@@ -57,8 +75,7 @@ end
 function inLoveParticles.drawParticles()
     --love.graphics.rectangle('fill', 50, 50, 1,1)
     for particleIndex, particle in ipairs(inLoveParticles.list) do
-        print('...')
-        love.graphics.rectangle('fill', particle.x, particle.y, 5,5)
+        love.graphics.rectangle('fill', particle.x, particle.y, 2,2)
     end
 end
 
