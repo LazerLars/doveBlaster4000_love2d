69ab110a8850fef9158558b7c66fa14486ebfdd4
Lars Mathiesen <larswmat@gmail.com>
2024-02-26 20:07:50 +0100

    simple drop move working

--- UtillityFunctions/inLoveParticles.lua
+++ UtillityFunctions/inLoveParticles.lua
@@ -5,7 +5,7 @@ local inLoveParticles = {}
 function inLoveParticles.load()
     inLoveParticles.list = {} -- list of particles
     inLoveParticles.createSimple(50, 50)
-    inLoveParticles.createSimple(25, 50)
+    inLoveParticles.createSimple(25, 25)
 end
 
 function inLoveParticles.update(dt)
@@ -35,10 +35,17 @@ end
 function inLoveParticles.simpleDropMove(dt)
     --todo: make particle go a little up, a little left and then drop...
     for particleIndex, particle in ipairs(inLoveParticles.list) do
-        particle.x = particle.x + 2 * dt
-        particle.y = particle.y + 100 * dt
         particle.duration = particle.duration - dt
-        print(particle.duration)
+        
+        if particle.duration >= 0.3 then
+            particle.x = particle.x - math.random(40,100) * dt
+            particle.y = particle.y - math.random(60,120) * dt
+        else
+            particle.x = particle.x - math.random(12,30) * dt
+            particle.y = particle.y + math.random(130,350) * dt
+
+        end
+
         if particle.duration <= 0 then
             print('die particle')
             inLoveParticles.removeParticle(particleIndex)
@@ -50,7 +57,7 @@ function inLoveParticles.drawParticles()
     --love.graphics.rectangle('fill', 50, 50, 1,1)
     for particleIndex, particle in ipairs(inLoveParticles.list) do
         print('...')
-        love.graphics.rectangle('fill', particle.x, particle.y, 1,1)
+        love.graphics.rectangle('fill', particle.x, particle.y, 5,5)
     end
 end
 
